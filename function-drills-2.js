c28d0cebb77bf66b3b4fbd282459ab18ea4b5e2d
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-10 16:26:18 -0500

    Created how many days function

--- function-drills-2.js
+++ function-drills-2.js
@@ -53,3 +53,36 @@ function decodeMessage(wholeMessage) {
 console.log(decode('cycle'));
 
 console.log(decodeMessage('craft block argon meter bells brown croon droop'));
+
+
+// How many days in a month
+function howManyDays(month, leapYear){
+  let result = '';
+  switch (month) {
+    case "January":
+    case "March":
+    case "May":
+    case "July":
+    case "August":
+    case "October":
+    case "December":
+      result = `${month} has 31 days`;
+      break;
+    case "April":
+    case "June":
+    case "September":
+    case "November":
+      result = `${month} has 30 days`;
+      break;
+    case "February":
+      result = leapYear ? `February has 29 days` : `February has 28 days`;
+      break;
+    default:
+      throw new Error('Must provide a valid month.');
+  }
+
+  return result;
+
+}
+
+console.log(howManyDays('February', true));
