5f029e1732cea90b5daa582baf21d4dd65babb32
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-10 15:58:27 -0500

    Created beyond function and added comments

--- function-drills-2.js
+++ function-drills-2.js
@@ -1,3 +1,22 @@
+// jediName
 const jediName = (firstName, lastName) => `${lastName.substring(0, 3)}${firstName.substring(0,2)}`;
 
 console.log(jediName('Beyonce', 'Knowles'));
+
+
+// beyond
+const beyond = num => {
+  if (isFinite(num)){
+    if (num > 0) {
+      return 'To infinity';
+    } else if (num < 0) {
+      return 'To negative infinity';
+    } else {
+      return 'Staying home';
+    }
+  } else {
+    return 'And beyond';
+  }
+}
+
+console.log(beyond(Infinity));
