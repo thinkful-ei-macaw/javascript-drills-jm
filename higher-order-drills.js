ec1c8cbc2970a03f55371fcd1b02d533531de6c4
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-10 16:59:07 -0500

    Created repeat, hello & goodbye functions

---
+++ higher-order-drills.js
@@ -0,0 +1,11 @@
+function repeat(fn,n){
+  for (let i = 0; i < n; i++) {
+    fn();
+  }
+}
+
+const hello = () => console.log('Hello world');
+const goodbye = () => console.log('Goodbye world');
+
+repeat(hello, 5);
+repeat(goodbye, 5);
