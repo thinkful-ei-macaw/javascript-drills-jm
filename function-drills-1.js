b2d7f97904b4114482d870744cd833b64183bc34
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-10 15:14:25 -0500

    added getYearOfBirth function and modified createGreeting function

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,7 +1,11 @@
+function getYearOfBirth(age) {
+    return 2020 - age;
+}
+
 function createGreeting(name, age) {
-    const yearOfBirth = 2020 - age;
+    const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Geroge', 29);
 console.log(greeting1);
